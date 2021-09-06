620d4ca8479a926c1c1723483b3cab4ae5c4bb43
Ritwik33 <ritwikawesome556@gmail.com>
2021-09-06 19:49:26 +0530

    Create ubandlb.cpp

---
+++ ubandlb.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    vector<int> arr(n);
+    for(int i = 0;i<n;i++) cin >> arr[i];
+    int x;
+    cin >> x;
+    cout << lower_bound(arr.begin(), arr.end(), x)-arr.begin() << endl;
+    cout << upper_bound(arr.begin(), arr.end(), x)-arr.begin() << endl;
+    return 0;
+}
