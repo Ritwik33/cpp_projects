389c3a1582ea4a5cdae3c65b92c6e7b6b4e04bd5
Ritwik33 <ritwikawesome556@gmail.com>
2021-09-11 13:19:03 +0530

    Create gcd.cpp

---
+++ gcd.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int gcd(int a, int b) {
+    if(b == 0) return a;
+    else return gcd(b, a%b);
+}
+
+int main() {
+    int a, b;
+    cout << "Enter numbers: ";
+    cin >> a >> b;
+    cout <<"The gcd is: "<< gcd(a,b) << endl; // recursive version of gcd
+    cout << __gcd(a,b) << endl; // stl function for gcd
+    return 0;
+}
