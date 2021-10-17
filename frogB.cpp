34a0691141be17ea2097d6c142a6c04196967787
Ritwik33 <ritwikawesome556@gmail.com>
2021-10-17 14:26:43 +0530

    Update frogB.cpp

--- frogB.cpp
+++ frogB.cpp
@@ -13,6 +13,20 @@ int frogB(int ind, int n, int k, vector<int>& dp, vector<int>& arr) {
     return dp[ind] = jump;
 }
 
+void printPath(vector<int> &dp, vector<int> &arr, int n, int k) {
+    int ind = 0;
+    cout << 0 << " ";
+    while(ind != n-1) {
+        for(int step = 1;step<=k;step++) {
+            if(dp[ind] - dp[ind+step] == abs(arr[ind] - arr[ind+step])) {
+                ind += step;
+                break;
+            }
+        }
+        cout << ind << " ";
+    }
+}
+
 int main() {
     int n;
     cin >> n;
@@ -22,5 +36,6 @@ int main() {
     int k;
     cin >> k;
     cout << frogB(0, n, k, dp, arr) << "\n";
+    printPath(dp, arr, n, k);
     return 0;
 }
