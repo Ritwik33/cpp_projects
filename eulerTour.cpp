9377995f6e9723849103fb30d581a95715db66d8
Ritwik33 <ritwikawesome556@gmail.com>
2021-11-30 14:49:00 +0530

    Create eulerTour.cpp

---
+++ eulerTour.cpp
@@ -0,0 +1,67 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define vi vector<int>
+#define rep(i, a, b) for(int i = a;i<b;i++)
+#define repd(i, a) for(int i = a;i>=0;i--)
+
+vi adj[100005];
+void inputTree() {
+    int n;
+    cin >> n;
+    rep(i, 1, n) {
+        int u, v;
+        cin >> u >> v;
+        adj[u].push_back(v);
+        adj[v].push_back(u);
+    }
+}
+
+int level[100005];
+vi eulerTour;
+void dfs(int node = 1, int par = 0, int lev = 1) {
+    level[node] = lev;
+    eulerTour.push_back(node);
+    for(auto it:adj[node]) {
+        if(it != par) {
+            dfs(it, node, lev+1);
+            eulerTour.push_back(node);
+        }
+    }
+}
+
+int firstOccurence[100005];
+void formFirstOccurence() {
+    memset(firstOccurence, -1, sizeof firstOccurence);
+    rep(i, 0, eulerTour.size()) {
+        if(firstOccurence[eulerTour[i]] == -1) firstOccurence[eulerTour[i]] = i;
+    }
+}
+
+int lca(int u, int v) {
+    int ind1, ind2;
+    ind1 = firstOccurence[u];
+    ind2 = firstOccurence[v];
+    int lca = level[eulerTour[ind1+1]];
+    rep(i, ind1+2, ind2) {
+        if(level[eulerTour[i]] < lca) {
+            lca = level[eulerTour[i]];
+        }
+    }
+    return lca;
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    inputTree();
+    dfs();
+    formFirstOccurence();
+    int q;
+    cin >> q;
+    while(q--) {
+        int u, v;
+        cin >> u >> v;
+        cout << lca(u, v) << "\n";
+    }
+    return 0;
+}
