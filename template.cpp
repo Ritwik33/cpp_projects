f85a1d27519c45c29b1aedd10d7e321658158e8e
Ritwik33 <ritwikawesome556@gmail.com>
2021-12-02 23:44:24 +0530

    Create template.cpp

---
+++ template.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define vi vector<int>
+#define vi2 vector<vector<int>>
+#define rep(i, a, b) for(int i = a;i<b;i++)
+#define repd(i, a) for(int i = a;i>=0;i--)
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    return 0;
+}
