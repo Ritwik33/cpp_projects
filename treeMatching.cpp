894150c1c902414389620afbd4903ecdec0f8277
Ritwik33 <ritwikawesome556@gmail.com>
2021-12-04 15:51:21 +0530

    Update treeMatching.cpp

--- treeMatching.cpp
+++ treeMatching.cpp
@@ -22,20 +22,23 @@ void dfs(int node = 1, int par = 0) {
     dp[node][0] = 0;
     dp[node][1] = 0;
     for(auto it:adj[node]) {
-        if(it != par) dfs(it, node);
+        if(it == par) continue;
+        dfs(it, node);
     }
     if(adj[node].size() == 1) return;
     for(auto it:adj[node]) {
-        if(it != par) {
-            dp[node][0] += max(dp[it][0], dp[it][1]);
-        }
+        if(it == par) continue;
+        dp[node][0] += max(dp[it][0], dp[it][1]);
     }
     for(auto picked:adj[node]) {
-        if(picked != par) {
-            int sum = 1 + dp[picked][0] + (dp[node][0] - max(dp[picked][0], dp[picked][1]));
-            dp[node][1] = max(dp[node][1], sum);
-        }
+        if(picked == par) continue;
+        int sum = 1 + dp[picked][0] + (dp[node][0] - max(dp[picked][0], dp[picked][1]));
+        dp[node][1] = max(dp[node][1], sum);
     }
+}
+
+void solve() {
+    cout << max(dp[1][0], dp[1][1]) << "\n";
 }
 
 int main() {
@@ -43,6 +46,6 @@ int main() {
     cin.tie(NULL);
     inputTree();
     dfs();
-    cout << max(dp[1][0], dp[1][1]) << "\n";
+    solve();
     return 0;
 }
