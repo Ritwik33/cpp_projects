8037bcd1c042650fa3736091436403c45d70376a
Ritwik33 <ritwikawesome556@gmail.com>
2021-12-04 15:04:59 +0530

    Update fibonacci.cpp

--- fibonacci.cpp
+++ fibonacci.cpp
@@ -1,18 +1,23 @@
 #include<bits/stdc++.h>
 using namespace std;
+#define vi vector<int>
+#define vi2 vector<vector<int>>
+#define rep(i, a, b) for(int i = a;i<b;i++)
+#define repd(i, a) for(int i = a;i>=0;i--)
 
-int fib(int n) {
-    if(n<=1) return n;
-    else {
-        return fib(n-1) + fib(n-2);
-    }
+int fib(int n, int dp[]) {
+    if(n <= 1) return dp[n] = n;
+    if(dp[n] != -1) return dp[n];
+    return dp[n] = fib(n-1, dp) + fib(n-2, dp);
 }
 
 int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
     int n;
     cin >> n;
-    for(int i = 0;i<=n;i++) {
-        cout<< fib(i) <<" ";
-    }
+    int dp[n+1];
+    memset(dp, -1, sizeof dp);
+    cout << fib(n, dp) << "\n";
     return 0;
 }
