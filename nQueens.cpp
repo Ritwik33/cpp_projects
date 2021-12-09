299cde37e8afe56b42ca69a9e31cb82791b84585
Ritwik33 <ritwikawesome556@gmail.com>
2021-12-09 23:40:02 +0530

    Update nQueens.cpp

--- nQueens.cpp
+++ nQueens.cpp
@@ -1,43 +1,53 @@
 #include<bits/stdc++.h>
 using namespace std;
+#define vi                                                         vector<int>
+#define vi2                                                        vector<vector<int>>
+#define rep(i, a, b)                                               for(int i = a;i<b;i++)
+#define repd(i, a)                                                 for(int i = a;i>=0;i--)
+#define setbits(x)                                                 __builtin_popcountll(x)
+#define int                                                        long long
+#define FIO                                                        ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
 
-bool isValid(int n, int row, int col, vector<char>& rowHash, vector<char>& udHash, vector<char>& ldHash) {
-    if(rowHash[row]=='Q' || udHash[n-1+row-col]=='Q' || ldHash[row+col]=='Q') return false;
+void show(vector<vector<char>>& board, int n) {
+    rep(i, 0, n) {
+        rep(j, 0, n) cout << board[i][j] << " ";
+        cout << "\n";
+    }
+}
+
+bool isPossible(int row, int col, bool rowhash[], bool udHash[], bool ldHash[], int n) {
+    if(rowhash[row] || udHash[n-1+row-col] || ldHash[row+col]) return false;
     return true;
 }
 
-bool nQueens(int col, int n, vector<vector<char>>& chess, vector<char>& rowHash, vector<char>& udHash, vector<char>& ldHash) {
+bool solve(int col, vector<vector<char>>& board, bool rowHash[], bool udHash[], bool ldHash[], int n) {
     if(col == n) {
-        for(int i = 0;i<n;i++) {
-            for(int j = 0;j<n;j++) cout << chess[i][j] << "  ";
-            cout << "\n\n";
-        }
+        show(board, n);
         return true;
     }
-    for(int row = 0;row<n;row++) {
-        if(isValid(n, row, col, rowHash, udHash, ldHash)) {
-            chess[row][col] = 'Q';
-            rowHash[row] = 'Q';
-            udHash[n-1+row-col] = 'Q';
-            ldHash[row+col] = 'Q';
-            if(nQueens(col+1, n, chess, rowHash, udHash, ldHash)==true) return true;
-            chess[row][col] = '.';
-            rowHash[row] = '.';
-            udHash[n-1+row-col] = '.';
-            ldHash[row+col] = '.';
+    rep(row, 0, n) {
+        if(isPossible(row, col, rowHash, udHash, ldHash, n)) {
+            board[row][col] = 'Q';
+            rowHash[row] = ldHash[row+col] = udHash[n-1+row-col] = true;
+            if(solve(col+1, board, rowHash, udHash, ldHash, n)) return true;
+            rowHash[row] = ldHash[row+col] = udHash[n-1+row-col] = false;
+            board[row][col] = '.';
         }
     }
     return false;
 }
 
-int main() {
+signed main() {
+    FIO;
     int n;
-    cout << "Enter n: ";
     cin >> n;
-    vector<vector<char>> chess(n, vector<char>(n, '.'));
-    vector<char> rowHash(n, '.');
-    vector<char> udHash(2*n-1, '.');
-    vector<char> ldHash(2*n-1, '.');
-    nQueens(0, n, chess, rowHash, udHash, ldHash);
+    vector<vector<char>> board(n, vector<char>(n, '.'));
+    bool rowHash[n];
+    bool udHash[2*n - 2];
+    bool ldHash[2*n - 2];
+    memset(rowHash, false, sizeof rowHash);
+    memset(udHash, false, sizeof udHash);
+    memset(ldHash, false, sizeof ldHash);
+    solve(0, board, rowHash, udHash, ldHash, n);
     return 0;
 }
