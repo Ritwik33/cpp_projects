c743bf95c6ba32bc449a32f9474d9c8012170e20
Ritwik33 <ritwikawesome556@gmail.com>
2021-12-09 23:40:09 +0530

    Update template.cpp

--- template.cpp
+++ template.cpp
@@ -5,9 +5,10 @@ using namespace std;
 #define rep(i, a, b)                                               for(int i = a;i<b;i++)
 #define repd(i, a)                                                 for(int i = a;i>=0;i--)
 #define setbits(x)                                                 __builtin_popcountll(x)
+#define int                                                        long long
 #define FIO                                                        ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
 
-int main() {
+signed main() {
     FIO;
 
     return 0;
