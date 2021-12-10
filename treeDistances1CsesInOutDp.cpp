8af7c33867fc7c82c5341fcedc9fe5d2e8516852
Ritwik33 <ritwikawesome556@gmail.com>
2021-12-11 01:36:55 +0530

    Create treeDistances1CsesInOutDp.cpp

---
+++ treeDistances1CsesInOutDp.cpp
@@ -0,0 +1,69 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define vi                                                         vector<int>
+#define vi2                                                        vector<vector<int>>
+#define rep(i, a, b)                                               for(int i = a;i<b;i++)
+#define repd(i, a)                                                 for(int i = a;i>=0;i--)
+#define setbits(x)                                                 __builtin_popcountll(x)
+#define FIO                                                        ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+
+vi adj[2*100005];
+void inputTree(int n) {
+    rep(i, 1, n) {
+        int u, v;
+        cin >> u >> v;
+        adj[u].push_back(v);
+        adj[v].push_back(u);
+    }
+}
+
+int in[2*100005];
+int out[2*100005];
+void dfsIn(int node = 1, int par = 0) {
+    in[node] = 0;
+    for(auto child:adj[node]) {
+        if(child == par) continue;
+        dfsIn(child, node);
+        in[node] = max(in[node], 1 + in[child]);
+    }
+}
+
+void dfsOut(int node = 1, int par1 = 0, int par2 = 0) {
+    out[node] = 0;
+    int deepestExceptNode = 0;
+    for(auto child:adj[par1]) {
+        if(child == node || child == par2) continue;
+        deepestExceptNode = max(deepestExceptNode, 1 + in[child]);
+    }
+    if(par1 != 0) {
+        out[node] = 1 + max(out[par1], deepestExceptNode);
+    }
+    for(auto child:adj[node]) {
+        if(child == par1) continue;
+        dfsOut(child, node, par1);
+    }
+}
+
+int ans[2*100005];
+void dfsComputeAns(int node = 1, int par = 0) {
+    int mx1 = -1;
+    ans[node] = out[node];
+    for(auto child:adj[node]) {
+        if(child == par) continue;
+        dfsComputeAns(child, node);
+        if(in[child] > mx1) mx1 = in[child];
+    }
+    ans[node] = max(ans[node], 1 + mx1);
+}
+
+int main() {
+    FIO;
+    int n;
+    cin >> n;
+    inputTree(n);
+    dfsIn();
+    dfsOut();
+    dfsComputeAns();
+    rep(i, 1, n+1) cout << ans[i] << " ";
+    return 0;
+}
