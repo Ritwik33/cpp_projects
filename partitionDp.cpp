0ed6f02af888b2de7c1d643c37785e5b2285ea3f
Ritwik33 <ritwikawesome556@gmail.com>
2021-12-13 23:37:00 +0530

    Update partitionDp.cpp

--- partitionDp.cpp
+++ partitionDp.cpp
@@ -1,19 +1,19 @@
 #include<bits/stdc++.h>
 using namespace std;
+#define int                                                        long long
 #define vi                                                         vector<int>
-#define vi2                                                        vector<vector<int>>
+#define vvi                                                        vector<vector<int>>
 #define rep(i, a, b)                                               for(int i = a;i<b;i++)
 #define repd(i, a)                                                 for(int i = a;i>=0;i--)
 #define setbits(x)                                                 __builtin_popcountll(x)
 #define FIO                                                        ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
 
-int dp[1000][1000];
-int partition(int i, int j, int arr[]) {
+int partition(int i, int j, int arr[], vvi& dp) {
     if(i == j) return dp[i][j] = 0;
     if(dp[i][j] != -1) return dp[i][j];
     int mini = INT_MAX;
     rep(k, i, j) {
-        int operations = partition(i, k, arr) + partition(k+1, j, arr) + (arr[i-1]*arr[k]*arr[j]);
+        int operations = partition(i, k, arr, dp) + partition(k+1, j, arr, dp) + (arr[i-1]*arr[k]*arr[j]);
         mini = min(mini, operations);
     }
     return dp[i][j] = mini;
@@ -25,11 +25,11 @@ void solve() {
     int arr[n];
     rep(i, 0, n) cin >> arr[i];
     int numOfMatrices = n-1;
-    memset(dp, -1, sizeof dp);
-    cout << partition(1, numOfMatrices, arr);
+    vvi dp(n, vi(n, -1));
+    cout << partition(1, numOfMatrices, arr, dp);
 }
 
-int main() {
+signed main() {
     FIO;
     solve();
     return 0;
