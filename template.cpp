cc3609b1ae65303206c2a5171ecc7657eb3b0c3b
Ritwik33 <ritwikawesome556@gmail.com>
2021-12-14 13:38:40 +0530

    Update template.cpp

--- template.cpp
+++ template.cpp
@@ -1,11 +1,13 @@
 #include<bits/stdc++.h>
 using namespace std;
+#define int                                                        long long
 #define vi                                                         vector<int>
-#define vi2                                                        vector<vector<int>>
+#define vvi                                                        vector<vector<int>>
+#define pb(x)                                                      push_back(x)
+#define ppb                                                        pop_back()
 #define rep(i, a, b)                                               for(int i = a;i<b;i++)
 #define repd(i, a)                                                 for(int i = a;i>=0;i--)
 #define setbits(x)                                                 __builtin_popcountll(x)
-#define int                                                        long long
 #define FIO                                                        ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
 
 signed main() {
