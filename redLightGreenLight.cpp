022359a63c83d24051e6cdd3ebe4adb66660e9cb
Ritwik33 <ritwikawesome556@gmail.com>
2021-12-22 23:51:18 +0530

    Create redLightGreenLight.cpp

---
+++ redLightGreenLight.cpp
@@ -0,0 +1,33 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define int                                                        long long
+#define vi                                                         vector<int>
+#define vvi                                                        vector<vector<int>>
+#define pb(x)                                                      push_back(x)
+#define ppb                                                        pop_back()
+#define rep(i, a, b)                                               for(int i = a;i<b;i++)
+#define repd(i, a)                                                 for(int i = a;i>=0;i--)
+#define setbits(x)                                                 __builtin_popcountll(x)
+#define FIO                                                        ios_base::sync_with_stdio(false); cin.tie(NULL);
+
+void solve() {
+    int t;
+    cin >> t;
+    while(t--) {
+        int n, k;
+        cin >> n >> k;
+        int cnt = 0;
+        rep(i, 0, n) {
+            int height;
+            cin >> height;
+            if(height > k) cnt++;
+        }
+        cout << cnt << "\n";
+    }
+}
+
+signed main() {
+    FIO;
+    solve();
+    return 0;
+}
