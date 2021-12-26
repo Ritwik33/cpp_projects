c4352df30fd74ebd7f11c787bdc5866461cf8bdd
Ritwik33 <ritwikawesome556@gmail.com>
2021-12-26 13:28:57 +0530

    Update romanticReversals.cpp

--- romanticReversals.cpp
+++ romanticReversals.cpp
@@ -10,15 +10,6 @@ using namespace std;
 #define setbits(x)                                                 __builtin_popcountll(x)
 #define FIO                                                        ios_base::sync_with_stdio(false); cin.tie(NULL);
 
-void reverseString(string& sPrime, int k) {
-    int l = 0;
-    while(l <= k) {
-        swap(sPrime[l], sPrime[k]);
-        l++;
-        k--;
-    }
-}
-
 void solve() {
     int t;
     cin >> t;
@@ -27,12 +18,24 @@ void solve() {
         cin >> n >> k;
         string sPrime;
         cin >> sPrime;
-        k--;
-        while(k >= 0) {
-            reverseString(sPrime, k);
-            k--;
+        vector<char> ans(sPrime.size());
+        int maxi = (k & 1) ? k : (k-1);
+        int cnt = 1, i = 0;
+        ans[k - cnt] = sPrime[i];
+        while(cnt != 2) {
+            cnt += (cnt & 1) ? 2:-2;
+            i++;
+            ans[k - cnt] = sPrime[i];
+            if(cnt == maxi) {
+                cnt += (k & 1) ? 1:3;
+            }
         }
-        cout << sPrime << '\n';
+        while(k < sPrime.size()) {
+            ans[k] = sPrime[k];
+            k++;
+        }
+        for(auto it:ans) cout << it;
+        cout << '\n';
     }
 }
 
