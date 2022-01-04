db3cbaa5fac59a5b45b2231358ccfcd13e4ebe08
Ritwik33 <ritwikawesome556@gmail.com>
2022-01-04 11:21:42 +0530

    Update bubblesort.cpp

--- bubblesort.cpp
+++ bubblesort.cpp
@@ -1,31 +1,41 @@
 #include<bits/stdc++.h>
 using namespace std;
+#define int                                                        long long
+#define vi                                                         vector<int>
+#define vvi                                                        vector<vector<int>>
+#define pb(x)                                                      push_back(x)
+#define ppb                                                        pop_back()
+#define rep(i, a, b)                                               for(int i = a;i<b;i++)
+#define repd(i, a)                                                 for(int i = a;i>=0;i--)
+#define setbits(x)                                                 __builtin_popcountll(x)
+#define FIO                                                        ios_base::sync_with_stdio(false); cin.tie(NULL);
+#define w(t)                                                       int t; cin >> t; while(t--)
 
-void sort(vector<int>& arr) {
-    for(int i = 0;i<arr.size()-1;i++) {
-        bool swapped = false;
-        for(int j = 0;j<arr.size() - i -1;j++) {
+int cnt = 0; // inversions
+void bubbleSort(vi& arr, int n) {
+    rep(i, 0, n) {
+        rep(j, 0, n-1) {
             if(arr[j] > arr[j+1]) {
-                int temp = arr[j];
-                arr[j] = arr[j+1];
-                arr[j+1] = temp;
-                swapped = true;
+                swap(arr[j], arr[j+1]);
+                cnt++;
             }
         }
-        if(!swapped) return;
     }
 }
 
-void display(vector<int>& arr) {
-    for(auto itr:arr) cout << itr << " ";
-}
-
-int main() {
+void solve() {
     int n;
     cin >> n;
-    vector<int> arr(n);
-    for(int i = 0;i<n;i++) cin >> arr[i];
-    sort(arr);
-    display(arr);
+    vi arr(n);
+    for(auto &it:arr) cin >> it;
+    bubbleSort(arr, n);
+    for(auto it:arr) cout << it << ' ';
+    cout << '\n';
+    cout << cnt; // total inversions
+}
+
+signed main() {
+    FIO;
+    solve();
     return 0;
 }
