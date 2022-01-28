58f7f03798e6ba95e5e977d407edcc20c93960e8
Ritwik33 <ritwikawesome556@gmail.com>
2022-01-28 13:52:27 +0530

    Update characterTrie.cpp

--- characterTrie.cpp
+++ characterTrie.cpp
@@ -16,6 +16,8 @@ using namespace std;
 #define w(q)                                                       int q; cin >> q; while(q--)
 #define FIO                                                        ios_base::sync_with_stdio(false); cin.tie(NULL);
 
+deque<char> dq;
+
 class node {
 public:
 	int pre, ends;
@@ -27,74 +29,83 @@ public:
 };
 
 class trie {
-	node *root;
 public:
+	node *root;
 	trie() {
 		root = new node();
 	}
 
 	void insert(const string& word) {
-		node *curr = root;
-		for (char ch : word) {
-			curr -> pre++;
-			if (!curr -> child[ch]) {
-				curr -> child[ch] = new node();
-			}
-			curr = curr -> child[ch];
+		node *cur = root;
+		for (char letter : word) {
+			cur -> pre++;
+			if (cur -> child[letter] == NULL) cur -> child[letter] = new node();
+			cur = cur -> child[letter];
 		}
-		curr -> pre++, curr -> ends++;
+		cur -> pre++, cur -> ends++;
 	}
 
-	int count(const string& word) {
-		node *curr = root;
-		for (char ch : word) {
-			if (!(curr -> child).count(ch)) return 0;
-			curr = curr -> child[ch];
+	int occurences(const string& word) {
+		node *cur = root;
+		for (char letter : word) {
+			if ((cur -> child).count(letter) == 0) return 0;
+			cur = cur -> child[letter];
 		}
-		return curr -> ends;
+		return cur -> ends;
 	}
 
 	int startsWith(const string& prefix) {
-		node *curr = root;
-		for (char ch : prefix) {
-			if (!(curr -> child).count(ch)) return 0;
-			curr = curr -> child[ch];
+		node *cur = root;
+		for (char letter : prefix) {
+			if ((cur -> child).count(letter) == 0) return 0;
+			cur = cur -> child[letter];
 		}
-		return curr -> pre;
+		return cur -> pre;
 	}
 
-	bool erase(const string& word) {
-		if (!count(word)) return false;
-		node *curr = root;
-		for (char ch : word) {
-			curr -> pre--;
-			curr = curr -> child[ch];
+	bool del(const string& word) {
+		node *cur = root;
+		if (occurences(word) == 0) return false;
+		for (char letter : word) {
+			cur -> pre--;
+			cur = cur -> child[letter];
 		}
-		curr -> pre--, curr -> ends--;
+		cur -> pre--, cur -> ends--;
 		return true;
 	}
 
-	node *erase_aux(node *curr, const string& word, int i) {
-		curr -> pre--;
+	node *del_aux(node *cur, const string& word, int i) {
+		cur -> pre--;
 		int n = word.length();
-		if (i == n - 1) curr -> ends--;
-		else {
-			curr -> child[word[i + 1]] = erase_aux(curr -> child[word[i + 1]], word, i + 1);
-			if (curr -> child[word[i + 1]] == NULL) {
-				(curr -> child).erase(word[i + 1]);
-			}
+		if (i == n - 1) {
+			cur -> ends--;
+		} else {
+			cur -> child[word[i + 1]] = del_aux(cur -> child[word[i + 1]], word, i + 1);
+			if (cur -> child[word[i + 1]] == NULL) (cur -> child).erase(word[i + 1]);
 		}
-		if (curr -> pre) return curr;
-		delete curr;
+		if (cur -> pre) {
+			return cur;
+		}
+		delete cur;
 		return NULL;
 	}
 
-	bool erase_actual(const string& word) {
-		if (!count(word)) return false;
-		root -> child[word[0]] = erase_aux(root -> child[word[0]], word, 0);
+	bool del_actual(const string& word) {
+		if (occurences(word) == 0) return false;
+		root -> child[word[0]] = del_aux(root -> child[word[0]], word, 0);
 		return true;
 	}
 
+	void printTrie(node *cur) {
+		if (cur -> ends) {
+			cout << endl;
+			return;
+		}
+		for (auto it : (cur -> child)) {
+			cout << it.first;
+			printTrie(it.second);
+		}
+	}
 };
 
 void solve() {
