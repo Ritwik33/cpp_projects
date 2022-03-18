0ada6945cc2ffe40c70f6e0bc1962c62eba81607
Ritwik33 <ritwikawesome556@gmail.com>
2022-03-18 13:39:21 +0530

    Create grid_paths.cpp

---
+++ dynamicProgramming/grid_paths.cpp
@@ -0,0 +1,64 @@
+// https://cses.fi/problemset/result/3690710/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+#define endl                                                       '\n'
+#define inf                                                        1e18
+#define mod                                                        1000000007
+#define int                                                        long long
+#define vi                                                         vector<int>
+#define pii                                                        pair<int, int>
+#define ff                                                         first
+#define ss                                                         second
+#define mii                                                        map<int, int>
+#define umii                                                       unordered_map<int, int>
+#define mp 														   make_pair
+#define vvi                                                        vector<vector<int>>
+#define pb(x)                                                      push_back(x)
+#define ppb                                                        pop_back()
+#define rep(i, a, b)                                               for(int i = a;i<b;i++)
+#define repd(i, a)                                                 for(int i = a;i>=0;i--)
+#define setbits(x)                                                 __builtin_popcountll(x)
+#define ps(x, y)                                                   fixed << setprecision(y) << x
+#define w(q)                                                       int q; cin >> q; while(q--)
+#define FIO                                                        ios_base::sync_with_stdio(false); cin.tie(NULL)
+
+void solve() {
+	int n; cin >> n;
+	vvi dp(n, vi(n));
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < n; j++) {
+			char c; cin >> c;
+			if (c == '*') continue;
+			if (i == 0 and j == 0) {
+				dp[i][j] = 1;
+			}
+			if (j - 1 >= 0) {
+				dp[i][j] += dp[i][j - 1];
+				dp[i][j] %= mod;
+			}
+			if (i - 1 >= 0) {
+				dp[i][j] += dp[i - 1][j];
+				dp[i][j] %= mod;
+			}
+		}
+	}
+	cout << dp[n - 1][n - 1] << endl;
+}
+
+int32_t main() {
+	FIO;
+
+// #ifndef ONLINE_JUDGE
+// 	freopen("input.txt", "r", stdin);
+// 	freopen("output.txt", "w", stdout);
+// #endif
+
+	int t = 1;
+	//cin >> t;
+	while (t--) {
+		solve();
+	}
+	return 0;
+}
