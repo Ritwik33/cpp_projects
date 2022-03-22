a146bbcf3c423f05864ee851242cc2d575a577cc
Ritwik33 <ritwikawesome556@gmail.com>
2022-03-22 14:24:23 +0530

    Create lis_nlogn.cpp

---
+++ dynamicProgramming/lis_nlogn.cpp
@@ -0,0 +1,56 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define endl                                                       '\n'
+#define inf                                                        1e18
+#define mod                                                        1000000007
+#define int                                                        long long
+#define vi                                                         vector<int>
+#define pii                                                        pair<int, int>
+#define ff                                                         first
+#define ss                                                         second
+#define mii                                                        map<int, int>
+#define umii                                                       unordered_map<int, int>
+#define mp 														   make_pair
+#define vvi                                                        vector<vector<int>>
+#define pb(x)                                                      push_back(x)
+#define ppb                                                        pop_back()
+#define rep(i, a, b)                                               for(int i = a;i<b;i++)
+#define repd(i, a)                                                 for(int i = a;i>=0;i--)
+#define setbits(x)                                                 __builtin_popcountll(x)
+#define ps(x, y)                                                   fixed << setprecision(y) << x
+#define w(q)                                                       int q; cin >> q; while(q--)
+#define FIO                                                        ios_base::sync_with_stdio(false); cin.tie(NULL)
+
+int len_of_lis(vi& arr, int n) {
+	vi vec;
+	vec.pb(arr[0]);
+	for (int i = 1; i < n; i++) {
+		if (vec.back() < arr[i]) vec.pb(arr[i]);
+		else vec[lower_bound(vec.begin(), vec.end(), arr[i]) - vec.begin()] = arr[i];
+	}
+	return vec.size();
+}
+
+void solve() {
+	int n; cin >> n;
+	vi x(n);
+	rep(i, 0, n) cin >> x[i];
+	cout << len_of_lis(x, n) << endl;
+}
+
+int32_t main() {
+	FIO;
+
+// #ifndef ONLINE_JUDGE
+// 	freopen("input.txt", "r", stdin);
+// 	freopen("output.txt", "w", stdout);
+// #endif
+
+	int t = 1;
+	//cin >> t;
+	while (t--) {
+		solve();
+	}
+	return 0;
+}
